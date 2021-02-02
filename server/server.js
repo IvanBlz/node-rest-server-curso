cb5c11e139ec8ac4cdb364fd68246d44d6c7d906
IvanBlz <ivanjhovanni@gmail.com>
2021-02-02 07:23:01 -0600

    Primer commit

---
+++ server/server.js
@@ -0,0 +1,45 @@
+require('./config/config.js');
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+
+app.get('/usuario', function(req, res) {
+    res.json('get Usuario')
+});
+
+app.post('/usuario', function(req, res) {
+    let body = req.body;
+    if (body.Nombre === undefined) {
+        res.status(400).json({
+            'ok': false,
+            'mensaje': 'Es necesario el nombre'
+        });
+    } else {
+
+        res.json({
+            persona: body
+        });
+    }
+});
+
+app.put('/usuario/:id', function(req, res) {
+    let id = req.params.id;
+    res.json({
+        id
+    });
+});
+
+app.delete('/usuario', function(req, res) {
+    res.json('delete Usuario')
+});
+
+app.listen(process.env.PORT, () => {
+    console.log("Escuchando en el puerto", process.env.PORT);
+});
